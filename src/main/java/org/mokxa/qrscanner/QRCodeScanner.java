7830a2de94068057222bd9b6b56a41ef2a85dc6f
jordan <jordan@mokxa.com>
2025-02-13 19:59:32 +0800

    Made it pom.xml work

--- src/main/java/org/mokxa/qrscanner/QRCodeScanner.java
+++ src/main/java/org/mokxa/qrscanner/QRCodeScanner.java
@@ -1,15 +1,22 @@
 package org.mokxa.qrscanner;
 
-import java.util.Map;
 import org.joget.apps.app.service.AppPluginUtil;
 import org.joget.apps.app.service.AppUtil;
 import org.joget.apps.form.lib.Radio;
 import org.joget.apps.form.lib.TextField;
 import org.joget.apps.form.model.FormBuilderPalette;
+import org.joget.apps.form.model.FormRow;
 import org.joget.apps.form.model.FormData;
 import org.joget.apps.form.service.FormUtil;
 import org.joget.apps.form.model.Element;
 import org.joget.apps.form.model.FormBuilderPaletteElement;
+
+import java.util.Map;
+import java.util.Collections;
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+import org.joget.commons.util.LogUtil;
 
 public class QRCodeScanner extends Element implements FormBuilderPaletteElement {
 
@@ -44,14 +51,15 @@ public class QRCodeScanner extends Element implements FormBuilderPaletteElement 
     @Override
     public String getLabel() {
         // support i18n
-        //return AppPluginUtil.getMessage("org.mokxa.QRCodeScanner.pluginLabel", getClassName(), MESSAGE_PATH);
-        return "QR Code Scanner";
+        return AppPluginUtil.getMessage("org.mokxa.QRCodeScanner.pluginLabel", getClassName(), MESSAGE_PATH);
+        //return "QR Code Scanner";
     }
 
     @Override
     public String getDescription() {
         // support i18n
-        return "QR Code Scanner";
+        return AppPluginUtil.getMessage("org.mokxa.QRCodeScanner.pluginDesc", getClassName(), MESSAGE_PATH);
+        //return "Form Field for scanning QR codes";
     }
 
     @Override
@@ -67,6 +75,42 @@ public class QRCodeScanner extends Element implements FormBuilderPaletteElement 
         String value = FormUtil.getElementPropertyValue(this, formData);
         dataModel.put("value", value);
 
+        
+        // String label = (String) getProperty("label");
+        // if (label == null || label.isEmpty()) {
+            //     label = getLabel(); // Default if missing
+            // }
+            // dataModel.put("label", label);
+
+            // Collection<Map> optionMap = new ArrayList<>();
+            
+            // FormRow fr = new FormRow();
+        // fr.setProperty("label", getLabel());
+        
+        // optionMap.add(fr);
+        
+        // // sort by numbering
+        // List<Map> sortedOptionMap = new ArrayList<>(optionMap);
+        
+        // Collections.sort(sortedOptionMap, (map1, map2) -> {
+            //     String numbering1 = (String) map1.get("numbering");
+            //     String numbering2 = (String) map2.get("numbering");
+            //     return numbering1.compareTo(numbering2); // For string-based sorting
+            // });
+            
+            // dataModel.put("options", sortedOptionMap);
+            
+        String label = getPropertyString("label"); // Fetch from properties.json
+        dataModel.put("label", getLabel());
+        
+        // Ensure element properties contain the label
+        if (getProperties() != null) {
+            getProperties().put("label", getLabel());
+        }
+        
+        LogUtil.info("QR Scanner", "dataModel" + dataModel);
+        LogUtil.info("QR Scanner", "getProperties()" + getProperties());
+        
         String html = FormUtil.generateElementHtml(this, formData, template, dataModel);
         return html;
     }
--- src/main/resources/templates/qrCodeScanner.ftl
+++ src/main/resources/templates/qrCodeScanner.ftl
@@ -1,30 +1,33 @@
 <div class="form-cell qr_code_scanner" ${elementMetaData!}>
-	<label field-tooltip="${elementParamName!}" class="label" for="${elementParamName!}">${element.properties.label} <span class="form-cell-validator">${decoration}</span><#if error??> <span class="form-error-message">${error}</span></#if></label>
-	<div class="form-cell-value"><span>${valueLabel!?html}</span></div>
-	<input id="${elementParamName!}" name="${elementParamName!}" type="hidden" value="${value!?html}"/>
-	<div id="qrCodeScanner" style="display: inline;"></div>
-	<style>
-		div#qrCodeScanner video.qrPreviewVideo {
-			width: 100%;
-			max-width: 600px;
-		}
-	</style>
-	<script type="text/javascript" src="${request.contextPath}/org.mokxa.qrscanner/js/jsqrscanner.nocache.js"></script>
-	<script type="text/javascript">
-		function onQRCodeScanned(scannedText)
-		{
-			$(FormUtil.getField(${elementParamName!})).val(scannedText).trigger("change");
-		}
-	
-		function JsQRScannerReady()
-		{
-			var jbScanner = new JsQRScanner(onQRCodeScanned);
-			jbScanner.setSnapImageMaxSize(600);
-			var scannerParentElement = document.getElementById("qrCodeScanner");
-			if(scannerParentElement)
-			{
-				jbScanner.appendTo(scannerParentElement);
-			}
-		}
-	</script>
+    <label field-tooltip="${elementParamName!}" class="label" for="${elementParamName!}">${element.properties.label} <span class="form-cell-validator">${decoration}</span><#if error??> <span class="form-error-message">${error}</span></#if></label>
+    <div class="form-cell-value"><span>${valueLabel!?html}</span>
+		<input id="${elementParamName!}" name="${elementParamName!}" type="hidden" value="${value!?html}"/>
+		<div id="scannerCam" style="display: inline;"></div>
+	</div>
+    <style>
+        div#scannerCam video.qrPreviewVideo {
+            width: 100%;
+            max-width: 600px;
+        }
+    </style>
+    <script type="text/javascript" src="${request.contextPath}/plugin/org.mokxa.qrscanner.QRCodeScanner/js/jsqrscanner.nocache.js"></script>
+    <script type="text/javascript">
+        function onQRCodeScanned(scannedText)
+        {
+            $(FormUtil.getField("${elementParamName!}")).val(scannedText).trigger("change");
+        }
+    
+        function JsQRScannerReady()
+        {
+			console.log("RANNNNN");
+            var jbScanner = new JsQRScanner(onQRCodeScanned);
+			console.log("test: jbScanner -" + jbScanner);
+            jbScanner.setSnapImageMaxSize(600);
+            var scannerParentElement = document.getElementById("scannerCam");
+            if(scannerParentElement)
+            {
+                jbScanner.appendTo(scannerParentElement);
+            }
+        }
+    </script>
 </div>
